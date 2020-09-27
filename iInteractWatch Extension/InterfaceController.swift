9aaebf76f741ead4630f4415fff8ada29135886a
jimzucker <jim@zucker.us>
2020-09-27 15:58:17 -0700

    Update file headers

--- iInteractWatch Extension/InterfaceController.swift
+++ iInteractWatch Extension/InterfaceController.swift
@@ -3,11 +3,11 @@
 //  iInteractWatch Extension
 //
 //  Created by Jim Zucker on 12/4/15.
-//  Copyright © 2015 
+//  Copyright © 2015 - 2020
 //
 //  This Source Code Form is subject to the terms of the Mozilla Public
 //  License, v. 2.0. If a copy of the MPL was not distributed with this
-//  file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/. 
 //
 
 import WatchKit
--- iInteractWatch Extension/NotificationController.swift
+++ iInteractWatch Extension/NotificationController.swift
@@ -3,7 +3,11 @@
 //  iInteractWatch Extension
 //
 //  Created by Jim Zucker on 9/27/20.
-//  Copyright © 2020 Strategic Software Engineering LLC. All rights reserved.
+//  Copyright © 2020 
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/. 
 //
 
 import WatchKit
